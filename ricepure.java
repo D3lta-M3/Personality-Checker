f8c4eedcabeaec4c216fa6c22966a2f51625e736
Diren Gomez <69670896+D3lta-M3@users.noreply.github.com>
2021-12-12 11:34:56 +0000

    ricepure.java is being edited so that it has somewhat of an implementation of the rice test.

---
+++ ricepure.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class ricepure{
+  private int ricepure_score;
+
+  public void ricepure_test(){
+    Scanner yesOrNo = new Scanner(System.in);
+    
+    ricepure_score = 100;
+
+    System.out.println("Have you ever went on a date with someone? \n y/n");
+    String q1 = yesOrNo.nextLine();
+
+    if (q1.charAt(0) == 'y'){
+      ricepure_score = ricepure_score - 5;
+
+      System.out.println("Have you ever dated someone? \n y/n");
+      String q1a = yesOrNo.nextLine();
+      if (q1a.charAt(0) == 'y'){
+        ricepure_score = ricepure_score - 5;
+      }
+      else if(q1a.charAt(0) == 'n'){}
+      else{
+        System.out.println("Invalid response recieved");
+        ricepure_score = 5000;
+      }
+    }
+    else if(q1.charAt(0) == 'n'){}
+    else{
+      System.out.println("Invalid response recieved");
+      ricepure_score = 5000;
+    }
+
+    System.out.println("Have you ever commited any form of crime? \n y/n");
+    String q2 = yesOrNo.nextLine();
+
+    if(q2.charAt(0) == 'y'){
+      ricepure_score = ricepure_score - 5;
+
+      System.out.println
+    }
+  }
+}
